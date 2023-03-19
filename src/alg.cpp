6c7e1f0a16f06981571415f70349b377fba02413
arkhipova9 <116936616+arkhipova9@users.noreply.github.com>
2023-03-19 16:41:05 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,23 +3,23 @@
 int cbinsearch(int *arr, int size, int value) {
   //  поместить сюда реализацию алгоритма
     int counter = 0;
-    int leftBorder = 0;
-    int rightBorder = size - 1;
-    while (rightBorder - leftBorder > 1) {
-        int midBorder = (leftBorder + rightBorder) / 2;
-        if (arr[midBorder] == value) {
+    int leftB = 0;
+    int rightB = size - 1;
+    while (rightB >= leftB) {
+        int midB = (leftB + rightB) / 2;
+        if (arr[midB] == value) {
             counter++;
-            for (int num = midBorder + 1; num <= rightBorder && arr[num] == value; num++) {
+            for (int n = midB + 1; n <= rightB && arr[n] == value; n++) {
                 counter++;
             }
-            for (int num = midBorder - 1; num >= leftBorder && arr[num] == value; num--) {
+            for (int n = midB - 1; n >= leftB && arr[n] == value; n--) {
                 counter++;
             }
             return counter;
-        } else if (arr[midBorder] < value) {
-            leftBorder = midBorder + 1;
-        } else {
-            rightBorder = midBorder - 1;
+        } else if (arr[midB] < value) {
+            leftB = midB + 1;
+        } else if (arr[midB] > value) {
+            rightB = midB - 1;
         }
     }
     return 0;
